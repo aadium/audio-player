80405f3fff37074e5c5c3b8c6da3d2ef41209ad4
Aadi Umrani <aadi.p.um@gmail.com>
2023-08-18 11:56:54 +0300

    Loading files from a csv now
    This removes the need to create a new element everytime.

---
+++ JavaScript/index_card.js
@@ -0,0 +1,46 @@
+fetch('../audioDetails.csv')
+    .then(response => response.text())
+    .then(text => {
+        const rows = text.split('\n').slice(1);
+        const container = document.getElementById('song-container');
+
+        for (let i = 0; i < rows.length; i += 2) {
+            const row = document.createElement('div');
+            row.className = 'song-row';
+
+            for (let j = 0; j < 2 && (i + j) < rows.length; j++) {
+                const [audio, image, song, artist] = rows[i + j].split(',');
+
+                const card = document.createElement('div');
+                card.className = 'card';
+
+                const link = document.createElement('a');
+                link.href = `pages/music_player.html?audio=${audio}&image=${image}&song=${encodeURIComponent(song)}&artist=${encodeURIComponent(artist)}`;
+                link.className = 'audio-link';
+
+                const img = document.createElement('img');
+                img.className = 'audio-image';
+                img.src = `images/${image}.jpg`;
+                img.alt = song;
+
+                const title = document.createElement('p');
+                title.className = 'audio-title';
+                title.textContent = song;
+
+                const artistName = document.createElement('p');
+                artistName.className = 'artist-name';
+                artistName.textContent = artist;
+
+                link.appendChild(img);
+                link.appendChild(title);
+                link.appendChild(artistName);
+                card.appendChild(link);
+                row.appendChild(card);
+            }
+
+            container.appendChild(row);
+        }
+    })
+    .catch(error => {
+        console.error('Error fetching or processing CSV:', error);
+    });
